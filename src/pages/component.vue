ebc6e73eff6010ce7821fa266cc24ce86613c4ad
butGrey <40267057+butGrey@users.noreply.github.com>
2019-07-04 15:50:02 +0800

    Create component.vue

---
+++ src/pages/component.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+    	<my-pagination :pagination="pagination"> </my-pagination>
+
+		<div>当前页：{{pagination.currentPage}}</div>
+		<div>总长度：{{pagination.totalItems}}</div>
+	</div>
+</template>
+<script>
+	export default{
+		name: "mycomponent",
+		data(){
+			return{
+				pagination: {
+					currentPage: 1,
+					totalItems: 10,
+					pageSize: 5,
+					type: 0,
+					pageNumChange: function (currentPage) {
+						console.log(currentPage);
+					},
+					pageUpdate: function (argument) {
+						// body...
+					},
+					pageOptionChange: function (argument) {
+						// body...
+					}
+				}
+			}
+		},
+		methods:{
+			doSomething: function(i){
+			  this.title+=i.time;
+			}
+		},
+	}
+</script>
