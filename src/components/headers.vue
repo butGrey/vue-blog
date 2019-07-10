6eddabcd023207e804699cc9d851b24e918ec26f
butGrey <40267057+butGrey@users.noreply.github.com>
2019-07-10 17:04:42 +0800

    Update headers.vue

--- src/components/headers.vue
+++ src/components/headers.vue
@@ -1,13 +1,14 @@
 <template>
 	<div class="header">
 		<ul>
-			<li style="font-size: 16px;">首页</li>
+			<li style="font-size: 16px;"><router-link to="home">首页</router-link></li>
 			<li>时光轴</li>
 			<li>留言板</li>
+			<li><router-link :to="{ path:'/component' , query: { key } }" class="button">组件</router-link></li>
 		</ul>
 		<ul class="login">
 			<li>登录</li>
-			<li>注册</li>
+			<li><router-link :to="{ path:'/signin' , query: { key } }" class="button">注册</router-link></li>
 			<li>退出</li>
 		</ul>
 	</div>
@@ -33,12 +34,15 @@
 		height: 50px;
 		line-height: 50px;
 		padding-left: 20px;
-		position: absolute;
-		color: #fff;
-		background-color: rgba(255,255,255,0);;
+		position: fixed;
+		top: 0;
+		color: #fff !important;
+		background-color: #37324a;
+		z-index: 999;
 		box-shadow: 2px 1px 10px rgba(146, 146, 146, 0.5);
 	}
-	ul, li{
+	ul, li, a{
+		color: #fff !important;
 		display: inline-block;
 		margin: 0 10px;
 	}
